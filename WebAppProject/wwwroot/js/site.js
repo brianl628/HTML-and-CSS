65245fbbc6fd1151bd0e18a7c80c21f2f9f406ec
brianl628 <118641307+brianl628@users.noreply.github.com>
2023-02-03 02:44:11 -0600

    Add files via upload

---
+++ WebAppProject/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function myFunction() {
+    document.getElementById("author").innerHTML = "This is Bohemian Rhapsody by Queen, made in 1975.";
+}
